07001839176df0c3c60d06ce2900efa91f838d03
fffffatah <ab.fatahmn@gmail.com>
2023-01-27 22:29:38 +0600

    add cascade delete for category

---
+++ Xpense/Models/ExpenseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Xpense.Models
+{
+    public class ExpenseViewModel
+    {
+        public long Id { get; set; }
+        public double Amount { get; set; }
+        public DateTime SpentAt { get; set; }
+        public string Category { get; set; }
+    }
+}
